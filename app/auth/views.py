a5d4f0cf46cd5e01e6345200591de877f9c6677d
V Melkov <v.melkov@gmail.com>
2021-10-13 00:48:55 +0500

    Added login logout pages User model has been changed

---
+++ app/auth/views.py
@@ -0,0 +1,31 @@
+from flask import render_template, request, url_for, redirect, flash, session
+from flask_login import login_required, login_user, logout_user, current_user
+
+from . import auth
+from .forms import LoginForm
+from ..models import User
+
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is not None and user.verify_password(form.password.data):
+            login_user(user, form.remember_me.data)
+            next = request.args.get('next')
+            if next is None or not next.startswith('/'):
+                next = url_for('main.index')
+            return redirect(next)
+        flash('Invalid username or password')
+    return render_template('auth/login.html', form=form)
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You have been logout')
+    return redirect(url_for('main.index'))
+
+
+
